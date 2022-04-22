78b6afcb628dde59499c8c316ad8d72e5349359a
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-04-22 15:21:01 +0200

    Update enterypoint.sh

--- enterypoint.sh
+++ enterypoint.sh
@@ -9,7 +9,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-* * * * * /run.sh >> /var/log/cron.log 2>&1
+15 * * * * python3 /withings2influxdb.py >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
