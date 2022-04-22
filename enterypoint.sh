772c19c842e2f2bff48b1f63a3f1ffc7b15cac13
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-04-22 15:19:04 +0200

    Create enterypoint.sh

---
+++ enterypoint.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container started"
+
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
+# Setup a cron schedule
+echo "SHELL=/bin/bash
+BASH_ENV=/container.env
+* * * * * /run.sh >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
