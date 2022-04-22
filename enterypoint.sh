a65f55a333a989b98e439275c0375dafbe8036f4
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-04-22 16:44:01 +0200

    change redirection

--- enterypoint.sh
+++ enterypoint.sh
@@ -10,7 +10,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-30 * * * * python3 /withings2influxdb.py > /dev/stdout
+50 * * * * python3 /withings2influxdb.py > /proc/1/fd/1 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
